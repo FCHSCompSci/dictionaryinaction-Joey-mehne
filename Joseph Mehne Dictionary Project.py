7a3c787444c8182c7400b7093520697c780b79c8
Joseph David Mehne <joseph.mehne@stu.jefferson.kyschools.us>
2018-09-18 16:14:45 -0400

    dictionary project updated

--- Joseph Mehne Dictionary Project.py
+++ Joseph Mehne Dictionary Project.py
@@ -1,44 +1,40 @@
 import random
 
-a_speed = input("What speed would you like the alien?(slow, medium, or fast): ")
+a_difficulty = input("What difficulty would you like the alien to be?(easy, medium, or hard): ")
 alien_0 = {
-    'x_position': 1,
-    'y_position': 26,
-    'speed': a_speed,
+    'difficulty': a_difficulty,
     'health': 12,
     }
-print("The alien is at " + str(alien_0['x_position']) + "," + str(alien_0['y_position']))
+print("The alien is coming for you!")
 
-if alien_0['speed'] == 'slow':
-    x_increment = 1
-    y_increment = 1
-elif alien_0['speed'] == 'medium':
-    x_increment = 2
-    y_increment = 2
-else:
-    x_increment = 3
-    y_increment = 3
-
-alien_0['x_position'] = alien_0['x_position'] + x_increment
-alien_0['y_position'] = alien_0['y_position'] + y_increment
+game_stats = {
+    'state': True,
+    }
 
 dice_list = [0,1,2,3,4,5,6,7,8,9,10,11,12]
-dice_roll = random.choice(dice_list)
-player_damage = dice_roll
-
-alien_0['health'] = alien_0['health'] - player_damage
 
 while True:
+    dice_roll = random.choice(dice_list)
     dice = input("Press r to roll the dice! Roll high enough and you'll get the alien!: ")
     if dice_roll == 12:
+        alien_0['health'] = alien_0['health'] - dice_roll
         print("Your rolled a 12! The alien is defeated!")
         break
-    elif dice_roll >= 7 and not dice_roll == 12:
-        print("Great shot! He should be dead soon!")
-    elif dice_roll >= 1 and not dice_roll == 7:
-        print("Not bad, but the alien isn't finished quite yet!")
+    elif dice_roll >= 7 and dice_roll <= 11:
+        alien_0['health'] = alien_0['health'] - dice_roll
+        print("You rolled a %s, great shot! He should be dead soon!" % dice_roll)
+    elif dice_roll >= 1 and dice_roll <= 6:
+        alien_0['health'] = alien_0['health'] - dice_roll
+        print("You rolled a %s. Not bad, but the alien isn't finished quite yet!" % dice_roll)
     else:
-        print("You rolled a 0 and missed your shot! It's now at " + str(alien_0['x_position']) + "," + str(alien_0['y_position']))
+        print("You rolled a 0 and missed your shot!")
+
+    if game_stats['state'] == False:
+        break
+
+
+
+
 
 
 #change speed to heatlh
