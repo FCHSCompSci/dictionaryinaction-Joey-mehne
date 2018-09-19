8f3521a161efb5a03b4266a59f9fea05dc321edd
Joseph David Mehne <joseph.mehne@stu.jefferson.kyschools.us>
2018-09-19 08:30:44 -0400

    dictionary project updated

--- Joseph Mehne Dictionary Project.py
+++ Joseph Mehne Dictionary Project.py
@@ -13,22 +13,28 @@ game_stats = {
 
 dice_list = [0,1,2,3,4,5,6,7,8,9,10,11,12]
 
+def alien_damage():
+    alien_0['health'] = alien_0['health'] - dice_roll
+
+#def new_health(alien,roll):
+    
 while True:
     dice_roll = random.choice(dice_list)
     dice = input("Press r to roll the dice! Roll high enough and you'll get the alien!: ")
     if dice_roll == 12:
-        alien_0['health'] = alien_0['health'] - dice_roll
+        alien_damage()
         print("Your rolled a 12! The alien is defeated!")
         break
     elif dice_roll >= 7 and dice_roll <= 11:
-        alien_0['health'] = alien_0['health'] - dice_roll
+        alien_damage()
         print("You rolled a %s, great shot! He should be dead soon!" % dice_roll)
     elif dice_roll >= 1 and dice_roll <= 6:
-        alien_0['health'] = alien_0['health'] - dice_roll
+        alien_damage()
         print("You rolled a %s. Not bad, but the alien isn't finished quite yet!" % dice_roll)
     else:
         print("You rolled a 0 and missed your shot!")
 
+while alien_0['health'] <= 0:
     if game_stats['state'] == False:
         break
 
