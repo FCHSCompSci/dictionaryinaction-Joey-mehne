e800ff6706e990b3685ace77f9130f0ef3e85102
Joseph David Mehne <joseph.mehne@stu.jefferson.kyschools.us>
2018-09-19 16:29:58 -0400

    dictionary project updated

--- Joseph Mehne Dictionary Project.py
+++ Joseph Mehne Dictionary Project.py
@@ -1,30 +1,43 @@
 import random
+import time
 
-a_difficulty = input("What difficulty would you like the alien to be?(easy, medium, or hard): ")
 alien_0 = {
-    'difficulty': a_difficulty,
-    'health': 12,
+    'health': 25,
     }
-print("The alien is coming for you!")
+time.sleep(3)
+print("The aliens are coming for Earth! Act fast or the human race is doomed! Seems like they're controlled by a mothership. Get the dice of power and use your training to...")
+print("")
+time.sleep(4)
+print("KILL...")
+print("")
+time.sleep(1.25)
+print("THEM...")
+print("")
+time.sleep(1.25)
+print("ALL!...")
+print("")
+time.sleep(1)
 
 game_stats = {
     'state': True,
     }
 
 dice_list = [0,1,2,3,4,5,6,7,8,9,10,11,12]
+my_rolls = 0
+alien_rolls = 0
 
 def alien_damage():
     alien_0['health'] = alien_0['health'] - dice_roll
 
-#def new_health(alien,roll):
-    
+def new_health(alien):
+    print("The alien's health has dropped to %s." % alien_damage)
+
 while True:
     dice_roll = random.choice(dice_list)
-    dice = input("Press r to roll the dice! Roll high enough and you'll get the alien!: ")
+    dice = input("Press r to roll the dice of power! Roll high enough and we'll be able to defeat the alien!: ")
     if dice_roll == 12:
         alien_damage()
-        print("Your rolled a 12! The alien is defeated!")
-        break
+        print("You rolled a 12! A critical hit if I've ever seen one!")
     elif dice_roll >= 7 and dice_roll <= 11:
         alien_damage()
         print("You rolled a %s, great shot! He should be dead soon!" % dice_roll)
@@ -32,12 +45,15 @@ while True:
         alien_damage()
         print("You rolled a %s. Not bad, but the alien isn't finished quite yet!" % dice_roll)
     else:
-        print("You rolled a 0 and missed your shot!")
+        print("You rolled a 0 and missed your shot! ")
 
-while alien_0['health'] <= 0:
-    if game_stats['state'] == False:
+    if alien_0['health'] <= 0:
+        print("*" * 120)
+        print("*" * 120)
+        print("*" * 120)
+        print("Congrats! The alien lays bloody on the ground defeated. The human race has been saved by you brave soldier!")
+        print("You Win!")
         break
-
 
 
 
